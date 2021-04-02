e9858d22808ec964a0a7eb9a1500a03af5513270
Jasnini <1041259804@qq.com>
2021-04-03 00:21:48 +0800

    test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -13,28 +13,26 @@ module.exports = {
             { text: '主页', link: '/' },
             { text: '博文',
               items: [
-                { text: 'Android', link: '/android/' },
-                { text: 'ios', link: '/ios/' },
-                { text: 'Web', link: '/web/' }
+                { text: '前端技术', link: '/frontEnd/' }
               ] 
             },
             { text: '关于', link: '/about/' },
             { text: 'Github', link: 'https://github.com/Jasnini' },
         ],
         sidebar: {
-            '/android/': [
-                        "",
-                        "android1", 
-                ],
-                "/ios/":[
-                        "",
-                        "ios1",
-                        ],
-                "/web/":[
-                        "",
-                        "web1",
-                ],
-            },
+            '/frontEnd/': [
+                {
+                    title: '前端技术总结',
+                    sidebarDepth: 2,
+                    collapsable: true,
+                    children: [
+                        ["/frontEnd/如何学好前端", "如何学好前端"],
+                        ["/frontEnd/网站性能优化", "网站性能优化"],
+                        ["/frontEnd/网页布局", "网页布局"]
+                    ]
+                }
+            ]
+        },
         sidebarDepth: 2,
         serviceWorker: true,
     },
