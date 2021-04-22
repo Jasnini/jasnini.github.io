aff954017f382b835ec40f0e8a68129d1846f075
Jasnini <1041259804@qq.com>
2021-04-23 00:13:59 +0800

    add article

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,27 +15,44 @@ module.exports = {
             { text: '主页', link: '/' },
             { text: '博文',
               items: [
-                { text: '前端技术', link: '/frontEnd/' }
+                { text: '前端技术', link: '/frontEnd/' },
+                {text: '机器学习', link: '/ml/'}
               ] 
             },
             { text: '关于', link: '/about/' },
             { text: 'Github', link: 'https://github.com/Jasnini' },
         ],
-        sidebar: {
+        sidebar: [{
             '/frontEnd/': [
                 {
                     title: '前端技术总结',
-                    sidebarDepth: 2,
+                    // sidebarDepth: 2,
                     collapsable: true,
                     children: [
                         ["/frontEnd/如何学好前端", "如何学好前端"],
                         ["/frontEnd/网站性能优化", "网站性能优化"],
-                        ["/frontEnd/网页布局", "网页布局"]
+                        ["/frontEnd/BFC", "BFC"],
+                        ["/frontEnd/CSS3新特性", "CSS3新特性"],
+                        ["/frontEnd/ES6新特性", "ES6新特性"],
+                        ["/frontEnd/H5新特性", "H5新特性"],
+                        ["/frontEnd/动画", "动画"],
                     ]
                 }
             ]
         },
-        sidebarDepth: 2,
+        {
+            '/ml/': [
+                {
+                    title: '机器学习',
+                    // sidebarDepth: 2,
+                    collapsable: true,
+                    children: [
+                        ["/ml/机器学习早知道", "机器学习早知道"]
+                    ]
+                }
+            ]
+        }],
+        // sidebarDepth: 2,
         serviceWorker: true,
     },
 }
