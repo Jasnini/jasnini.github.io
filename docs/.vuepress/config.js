43fb919436b8876f669c0201d77810daf0f952f2
Jasnini <1041259804@qq.com>
2021-05-21 21:00:07 +0800

    add fancybox for img

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,11 @@ module.exports = {
     dest: './blog',
     port: '32087',
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }]
+        ['link', { rel: 'icon', href: '/favicon.ico' }],
+        // add jquert and fancybox
+        ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js' }],
+        ['script', { src: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.js' }],
+        ['link', { rel: 'stylesheet', type: 'text/css', href: 'https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.2/jquery.fancybox.min.css' }]
     ],
     lastUpdated: 'Last Updated',
     themeConfig: {
--- docs/frontEnd/ES6新特性.md
+++ docs/frontEnd/ES6新特性.md
@@ -1,7 +1,8 @@
 # ES6新特性
 #### 新增let和const变量
 所有新增特性总结如下：  
- <img :src="$withBase('/ES6.jpg')" alt="mul-front">
+ <!-- <img :src="$withBase('/ES6.jpg')" alt="mul-front"> -->
+ <a data-fancybox title="" href="/ES6.jpg">![](/ES6.jpg)</a>
 <center>图2 ES6</center>
 - 块级作用域
 - 暂死区
